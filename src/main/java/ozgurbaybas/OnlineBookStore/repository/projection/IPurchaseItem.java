743c133849def4410672ae79db844411eedb5d62
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-07 15:07:33 +0300

    Implementation and Projection of Purchase Repository Class with Spring Data JPA

---
+++ src/main/java/ozgurbaybas/OnlineBookStore/repository/projection/IPurchaseItem.java
@@ -0,0 +1,10 @@
+package ozgurbaybas.OnlineBookStore.repository.projection;
+
+import java.time.LocalDateTime;
+
+public interface IPurchaseItem {
+
+    String getTitle();
+    Double getPrice();
+    LocalDateTime getPurchaseTime();
+}
