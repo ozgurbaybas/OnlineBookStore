7bff01c89637b62013e4d82e04f3d3198dd255c0
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-08 03:36:48 +0300

    Creation of Authentication Controller Class completed

---
+++ src/main/java/ozgurbaybas/OnlineBookStore/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package ozgurbaybas.OnlineBookStore.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ozgurbaybas.OnlineBookStore.model.User;
+import ozgurbaybas.OnlineBookStore.service.IAuthenticationService;
+import ozgurbaybas.OnlineBookStore.service.IUserService;
+
+@RestController
+@RequestMapping("api/authentication") //pre-path
+public class AuthenticationController
+{
+    @Autowired
+    private IAuthenticationService authenticationService;
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("sign-up") // api/authentication/sign-up
+    public ResponseEntity<User> signUp(@RequestBody User user)
+    {
+        if(userService.findByUsername(user.getUsername()).isPresent()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping("sign-in") // api/authentication/sign-in
+    public ResponseEntity<?> signIn(@RequestBody User user)
+    {
+        return new ResponseEntity<>(authenticationService.signInAndReturnJWT(user),HttpStatus.OK);
+    }
+}
